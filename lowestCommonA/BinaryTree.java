862ad0d9d526c4eba6cb04f27cef3910398afc1f
Nualaod <nidhomnu@tcd.ie>
2018-10-02 15:29:00 +0100

    lowestCommonAncestor function finished

--- lowestCommonA/BinaryTree.java
+++ lowestCommonA/BinaryTree.java
@@ -67,20 +67,50 @@ public class BinaryTree
 	    return current;
 	}
 
-
+ public	Node lowestCommonAncestor(int node1, int node2)
+	    {
+	        return lowestCommonAncestor(root, node1, node2);
+	    }
 
 	// find lowest common ancestor of two nodes
-	public Node lowestCommonAncestor(Node root, Node node1, Node node2)
-	{
-		if(!containsNode(node1.value)||!containsNode(node2.value))
-		{
-			return null;
-		}
-		if(root == null)
-		{
-			return null;
-		}
-		return null;
+	private Node lowestCommonAncestor(Node root, int node1, int node2)
+	 {
+			 // Base case
+			 if (root == null)
+					 return null;
+
+			 // If either n1 or n2 matches with root's key, report
+			 // the presence by returning root (Note that if a key is
+			 // ancestor of other, then the ancestor key becomes LCA
+			 if (root.value == node1 || root.value == node2)
+					 return root;
+
+			 // Look for keys in left and right subtrees
+			 Node leftLCA = lowestCommonAncestor(root.left, node1, node2);
+			 Node rightLCA = lowestCommonAncestor(root.right, node1, node2);
+
+			 // If both of the above calls return Non-NULL, then one key
+			 // is present in once subtree and other is present in other,
+			 // So this node is the LCA
+			 if (leftLCA!=null && rightLCA!=null)
+					 return root;
+
+			 // Otherwise check if left subtree or right subtree is LCA
+			 return (leftLCA != null) ? leftLCA : rightLCA;
+	 }
+
+	public static void main(String[] args) {
+		BinaryTree theTree = new BinaryTree();
+		theTree.add(5);
+		theTree.add(7);
+		theTree.add(9);
+		theTree.add(2);
+		theTree.add(10);
+		theTree.add(4);
+		theTree.add(3);
+		theTree.add(8);
+
+		System.out.print("does theTree contain 2? " + theTree.containsNode(2));
 	}
 
 }
--- lowestCommonA/BinaryTreeTest.java
+++ lowestCommonA/BinaryTreeTest.java
@@ -12,7 +12,7 @@ public class BinaryTreeTest
 	public void testContains(){
 
 		BinaryTree theTree = new BinaryTree() ;
-		theTree.adds(1);
+		theTree.add(1);
 		assertTrue("Check bt contains value", theTree.contains(1));
 		assertFalse("Check bt for non-present value", theTree.contains(4));
 
@@ -21,5 +21,27 @@ public class BinaryTreeTest
 
 	}
 
+	@Test
+	public void testLowestCommonA(){
 
+		BinaryTree theTree = new BinaryTree() ;
+		theTree.add(5);
+		theTree.add(7);
+		theTree.add(9);
+		theTree.add(2);
+		theTree.add(10);
+		theTree.add(4);
+		theTree.add(3);
+		theTree.add(8);
+/*
+		Node actual = new Node(theTree.lowestCommonAncestor(8,10));
+		Node expected = new Node(7);
+		assertEquals("Check that the lca for 8 and 10 = 7", actual.int, expected.value);
+		assertFalse("Check bt for non-present value", theTree.contains(4));
+
+		BinaryTree empty = new BinaryTree();
+		assertFalse("Check empty bst", empty.contains(8));
+
+*/
+}
 }
